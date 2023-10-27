06d316cf8294570db253460a1f899e83d9e756bb
Yasin <wytm97@protonmail.com>
2023-10-28 00:20:37 +0100

    feat: update font and add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,18 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                sans: ['Nanum Gothic', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                "dark-gray": "#444444",
+                "dim-gray": "#6F6F6F",
+                "light-gray": "#0000001F",
+                "dark-blue": "#0D7AB5",
+                "dim-blue": "#64C0F3",
+                "light-blue": "#64C0F330",
+                "dim-purple": "#BBA4FB",
+                "light-purple": "#BBA4FB30",
+            }
         },
     },
     plugins: [
