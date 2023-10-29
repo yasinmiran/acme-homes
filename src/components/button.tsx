8ebbbf7d3dcd69938fe881ae5788d8f32ea62625
Yasin <wytm97@protonmail.com>
2023-10-29 20:16:49 +0000

    fix: button patch

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,5 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import classNames from "classnames";
 
 export interface ButtonProps extends PropsWithChildren<
     ButtonHTMLAttributes<HTMLButtonElement>
@@ -14,11 +15,13 @@ export default function Button(props: ButtonProps) {
     return (
         <button
             { ...rest }
-            type="button"
+            disabled={ inactive }
             className={
-                `rounded-md px-3 py-2 text-sm font-semibold shadow-sm bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${
-                    inactive ? "bg-gray-300/75 text-dim-gray" : ""
-                }`
+                classNames(
+                    "rounded-md px-3 py-2 text-sm disabled:opacity-40 font-semibold shadow-sm bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50", {
+                        "bg-gray-200 text-dim-gray pointer-events-none": inactive
+                    }
+                )
             }
         >
             { children }
