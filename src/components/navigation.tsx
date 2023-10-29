baee89905896d205aabaf6bcd850ee8b0c706f8d
Yasin <wytm97@protonmail.com>
2023-10-29 01:00:39 +0000

    fix: remove notification bell

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,5 +1,4 @@
 import { Disclosure } from '@headlessui/react'
-import { BellIcon } from '@heroicons/react/24/outline'
 import { GrHomeRounded } from "react-icons/gr";
 import UserProfile from "./user-profile.tsx";
 
@@ -16,16 +15,7 @@ export default function Navigation() {
                             </div>
                         </div>
                     </div>
-                    <div
-                        className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <button
-                            type="button"
-                            className="relative rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                        >
-                            <span className="absolute -inset-1.5"/>
-                            <span className="sr-only">View notifications</span>
-                            <BellIcon className="h-6 w-6" aria-hidden="true"/>
-                        </button>
+                    <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <UserProfile/>
                     </div>
                 </div>
