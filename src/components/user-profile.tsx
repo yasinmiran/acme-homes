f0e93a64f07f5d916408ac51e1716fe9304ac0e9
Yasin <wytm97@protonmail.com>
2023-10-29 01:42:43 +0100

    feat: style user profile dropdown

--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -6,13 +6,13 @@ export default function UserProfile() {
         <Menu as="div" className="relative ml-3">
             <div>
                 <Menu.Button
-                    className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                    className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-dark-blue focus:ring-offset-2">
                     <span className="absolute -inset-1.5"/>
                     <span className="sr-only">Open user menu</span>
                     <img
                         className="h-10 w-10 rounded-full"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
+                        alt="Profile Photo"
                     />
                 </Menu.Button>
             </div>
@@ -28,15 +28,17 @@ export default function UserProfile() {
                 <Menu.Items
                     className="absolute right-0 z-[50] mt-2 w-80 origin-top-right rounded-xl bg-white p-4 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none flex flex-col gap-y-4">
                     <Menu.Item>
-                        <div>
-                            <div>Jane Smith</div>
-                            <div>jane.smith@me.com</div>
+                        <div className="flex flex-col space-y-1">
+                            <p className="font-bold text-dark-gray">Jane Smith</p>
+                            <p className="text-dim-gray text-sm">jane.smith@me.com</p>
                         </div>
                     </Menu.Item>
                     <Menu.Item>
-                        <div>
-                            <a href="#">View Profile</a>
-                            <button>Sign out</button>
+                        <div className="flex items-center justify-between">
+                            <a href="#" className="text-dark-gray underline font-bold">View profile</a>
+                            <button className="px-4 py-2 bg-dark-blue text-white rounded-3xl text-sm font-bold">
+                                Sign out
+                            </button>
                         </div>
                     </Menu.Item>
                 </Menu.Items>
