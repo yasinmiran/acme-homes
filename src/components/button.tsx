2a94a5ca5ede802053f73fe4241c683c76f731b0
Yasin <wytm97@protonmail.com>
2023-10-29 19:57:02 +0000

    fix(Button): ignore `disabled` prop
    HTML button doesn't support disabled attribute.

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,12 +3,13 @@ import { ButtonHTMLAttributes, PropsWithChildren } from "react";
 export interface ButtonProps extends PropsWithChildren<
     ButtonHTMLAttributes<HTMLButtonElement>
 > {
+    inactive?: boolean;
     secondary?: boolean;
 }
 
 export default function Button(props: ButtonProps) {
 
-    const { children, ...rest } = props;
+    const { children, inactive, ...rest } = props;
 
     return (
         <button
@@ -16,7 +17,7 @@ export default function Button(props: ButtonProps) {
             type="button"
             className={
                 `rounded-md px-3 py-2 text-sm font-semibold shadow-sm bg-white text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ${
-                    rest.disabled ? "bg-gray-300/75 text-dim-gray" : ""
+                    inactive ? "bg-gray-300/75 text-dim-gray" : ""
                 }`
             }
         >
